35d9df2ff8ff8fac6ebc16300a12125c6b48102d
jose.ccna <jose.ccna@gmail.com>
2026-04-02 22:11:59 -0300

    Feito classe usuario e herança

---
+++ Aula/01/ControleEstoque/ControleEstoque.API/Models/Usuario.cs
@@ -0,0 +1,59 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ControleEstoque.API.Models
+{
+    public enum PerfilUsuario
+    {
+        Cliente,
+        Caixa,
+        Gerente,
+    }
+
+
+    public abstract class Usuario // abstract para não permitir instâncias Usuario diretas, apenas herança
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, StringLength(100)]
+        public string Nome { get; set; }
+
+        [Required, EmailAddress, StringLength(100)]
+        public string Email { get; set; }
+
+        [Required]
+        public string SenhaHash { get; set; } // aplicar tamanho mínimo 
+
+        [Required]
+        public PerfilUsuario Perfil { get; set; }
+
+
+    }
+
+    public class Cliente : Usuario
+    {
+        // Propriedades específicas para Cliente, se necessário
+        [StringLength(14)]
+        public string CPF { get; set; } // Exemplo de propriedade específica para Cliente
+
+    }
+
+
+    public class Caixa : Usuario
+    {
+        // Propriedades específicas para Caixa, se necessário
+        [StringLength(50)]
+        public string Turno { get; set; } // Exemplo de propriedade específica para Caixa
+    }
+
+
+    public class Gerente : Usuario
+    {
+        // Propriedades específicas para Gerente, se necessário
+        [StringLength(50)]
+        public string Setor { get; set; } // Exemplo de propriedade específica para Gerente
+
+    }   
+
+
+}
--- Aula/01/ControleEstoque/ControleEstoque.API/Program.cs
+++ Aula/01/ControleEstoque/ControleEstoque.API/Program.cs
@@ -1,3 +1,5 @@
+using ControleEstoque.API.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -23,3 +25,5 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+
